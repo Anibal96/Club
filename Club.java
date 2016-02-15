1634050bf59f224b7f1ebc2ed396ba37e7334fb0
unknown <you@example.com>
2016-02-15 11:24:14 +0100

    Añadido metodo que devuelve el numero de miembros que se dieron de alta el mes

--- Club.java
+++ Club.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 /**
  * Store details of club memberships.
  * 
@@ -8,13 +9,14 @@ import java.util.ArrayList;
 public class Club
 {
     private ArrayList<Membership> members;
-    
+    private int[] miembrosPorMes;
     /**
      * Constructor for objects of class Club
      */
     public Club()
     {
         members = new ArrayList<Membership>();
+        miembrosPorMes = new int[12];
     }
 
     /**
@@ -23,6 +25,8 @@ public class Club
      */
     public void join(Membership member)
     {
+        int mes = member.getMonth();
+        miembrosPorMes[mes-1] = miembrosPorMes[mes-1] + 1;
         members.add(member);
     }
 
@@ -34,4 +38,24 @@ public class Club
     {
         return members.size();
     }
+    
+    /** 
+    * Calcula el numero de socios que se dieron de alta en un mes determinado. 
+    * El año no nos importa. En caso de que el parametro contenga un valor 
+    * no valido se muestra por pantalla el error.
+    * @param month El mes en el que estamos interesados
+    * @return El numero de socios que se dieron de alta dicho mes
+    */
+    public String joinedMonth(int month)
+    {
+        String miembros;
+        if (month <= 12 && month >= 1){
+             miembros = miembrosPorMes[month-1] + "";
+        }
+        else{
+            miembros = "El número introduccido no corresponde con ningun mes.";
+        }
+        return miembros;
+    }
 }
+
